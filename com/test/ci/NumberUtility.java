59e2b881e74abf8a02c8debf7991573172d7d578
rbhat <rajeevmbhat@gmail.com>
2014-07-14 16:52:13 -0700

    Create NumberUtility.java

---
+++ com/test/ci/NumberUtility.java
@@ -0,0 +1,24 @@
+package com.test.ci;
+
+import java.util.*;
+
+public class NumberUtility{
+
+  public boolean isEven(int num){
+    return num%2==0;
+  }
+  
+  public int countEvens(int[] arr){
+  // add sonar violatios here
+    this.arr = arr;
+    int evenCount = 0;
+    
+    for(int i=0;i<arr.length; ++i){
+      if(arr[i]%2==0){
+        evenCount++;
+      }
+    }
+    
+    return evenCount;
+  }
+}
